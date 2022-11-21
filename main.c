0a8f2724e0781fe69c6c10a837647443d83dd2ba
tony-yu1 <63677210+tony-yu1@users.noreply.github.com>
2022-11-20 21:20:11 -0500

    Update main.c
    Updated User Code 0

--- main.c
+++ main.c
@@ -61,6 +61,69 @@ static void MX_TIM1_Init(void);
 
 /* Private user code ---------------------------------------------------------*/
 /* USER CODE BEGIN 0 */
+#define MPU6050_ADDR oxD0
+
+
+#define SMPLRT_DIV_REG 0x19
+#define GYRO_CONFIG REG 0x1B
+#define ACCEL_CONFIG REG 0x1C
+#define ACCEL_XOUT_H_REG 0x3B
+#define TEMP_OUT_H_REG 0x41
+#define GYRO_XOUT_H_REG 0x43
+#define PWR_MGMT_1_REG 0x6B
+#define WHO_AM_I_REG 0x75
+
+
+
+void MPU6050_Init (void) {
+	uint8_t check, Data;
+
+	//Check device ID WHO_AM_I
+
+	HAL_I2C_Mem_Read(&hi2c1, MPU6050_ADDR, WHO_AM_I_REG, 1, &check, 1, 1000);
+
+	if (check == 104) // if the device is present
+	{
+		// power management register OX6B we should write all 0's to wake the sensor up
+		Data = 0;
+		HAL_I2C_Mem_Write(&hi2cl, MPU6050_ADDR, PWR_MGMT_1_REG, 1, &Data, 1, 1000);
+
+		// Set DATA RATE of 1KHz by writing SMPLRT_DIV register
+		Data = 0x07;
+		HAL_I2C_Mem_Write(&hi2cl, MPU6050_ADDR, SMPLRT_DIV_REG, 1, &Data, 1, 1000);
+
+		// Set accelerometer configuration in ACCEL_CONFIG Register
+		// XA_ST=0,YA_ST=0,ZA ST=0, FS_SEL=0 -> + 2g
+
+		Data = 0x00;
+
+		HAL_I2C_Mem_Write(&hi2cl, MPU6050_ADDR, ACCEL_CONFIG_REG, 1, Data, 1, 1000);
+	}
+
+}
+
+void MPU6050_Read_Accel (void)
+{
+	uint8_t Rec_Data[6];
+	// Read 6 BYTES of data starting from ACCEL_XOUT_H register
+
+	HAL_I2C_Mem_Read (&hi2c1, MPU6050_ADDR, ACCEL_XOUT_H_REG, 1, Rec_Data, 6, 1000);
+
+	Accel_X_RAW = (int16_t)(Rec_Data[0] << 8 | Rec_Data [1]);
+	Accel_Y_RAW = (int16_t)(Rec_Data[2] << 8 | Rec_Data [3]);
+	Accel_Z_RAW = (int16_t)(Rec_Data[4] << 8 | Rec_Data [5]);
+
+	/*** convert the RAW values into acceleration in 'g
+	we have to divide according to the Full scale value set in FS_SEL
+	I have configured FS_SEL = 0. So I am dividing by 16384.0
+
+	for more details check ACCEL_CONFIG Register ****/
+
+	Ax = Accel_X_RAW/16384.0;
+	Ay = Accel_Y_RAW/16384.0;
+	Az = Accel_Z_RAW/16384.0;
+
+}
 
 /* USER CODE END 0 */
 
